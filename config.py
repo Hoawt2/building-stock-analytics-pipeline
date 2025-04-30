ba401c51a6dc885742d78c3a2b8d06c128544796
hoanglt2004 <hoangnt.b22tc048@gmail.com>
2025-04-30 20:19:35 +0700

    initial commit

--- config.py
+++ config.py
@@ -1,18 +1,11 @@
-# DB_CONFIG = {
-#     "host": "localhost",
-#     "port": 3306,
-#     "user": "root",
-#     "password": "hoang504901",
-#     "database": "stock_bi_test"
-# }
-API_KEY_FMP = "TPTxrDMbZB96m9QtFClX8kGTsgub1WM3"
-API_KEY_APV = "25L8UJNSALAN1R7G"
 import os
 
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST", "db"),
-    "port": int(os.getenv("DB_PORT", 3306)),
-    "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "hoang504901"),
-    "database": os.getenv("DB_NAME", "stock_bi")
+    "host": os.environ.get("DB_HOST", "localhost"),  # Lấy từ biến môi trường, mặc định là localhost nếu không có
+    "port": int(os.environ.get("DB_PORT", 3306)),    # Lấy từ biến môi trường, mặc định là 3306
+    "user": os.environ.get("DB_USER", "root"),      # Lấy từ biến môi trường, mặc định là root
+    "password": os.environ.get("DB_PASSWORD", "Hoang@112004"), # Lấy từ biến môi trường, mặc định
+    "database": os.environ.get("DB_NAME", "stock_bi") # Lấy từ biến môi trường, mặc định
 }
+API_KEY_FMP = os.environ.get("API_KEY_FMP", "sU2mjPeCsIB230uLzT5UcR257V8ljpnr") # Lấy từ biến môi trường, mặc định
+API_KEY_APV = os.environ.get("API_KEY_APV", "1OG5950ZT7NUWVOB") # Lấy từ biến môi trường, mặc định
